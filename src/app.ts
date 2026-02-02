3671632a5c6464052763a649d9a9c8499bff8242
shishir <mdabdulaziz3745@gmail.com>
2026-02-02 11:53:52 +0600

    initialized better-auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { type Application } from "express";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
   res.send("Pharma Care");
